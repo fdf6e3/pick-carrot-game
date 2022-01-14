3aad835fd35b0d60a2ab3eea90af86bc81ab4109
YOUNGHYUN BAE <yhbae97@gmail.com>
2022-01-14 20:50:29 +0900

    add functions for the sound effects

--- main.js
+++ main.js
@@ -15,6 +15,12 @@ const popUp = document.querySelector('.pop-up');
 const popUpText = document.querySelector('.pop-up__message');
 const popUpRefresh = document.querySelector('.pop-up__refresh');
 
+const carrotSound = new Audio('./sound/carrot_pull.mp3');
+const alertSound = new Audio('./sound/alert.wav');
+const bgSound = new Audio('./sound/bg.mp3');
+const bugSound = new Audio('./sound/bug_pull.mp3');
+const winSound = new Audio('./sound/game_win.mp3');
+
 let started = false;
 let score = 0;
 let timer = undefined;
@@ -39,6 +45,7 @@ function startGame() {
   showStopButton();
   showTimerAndScore();
   startGameTimer();
+  playSound(bgSound);
 }
 
 function stopGame() {
@@ -46,12 +53,20 @@ function stopGame() {
   stopGameTimer();
   hideGameButton();
   showPopUpWithText('REPLAY ?');
+  playSound(alertSound);
+  stopSound(bgSound);
 }
 
 function finishGame(win) {
   started = false;
-  stopGameTimer();
   hideGameButton();
+  if (win) {
+    playSound(winSound);
+  } else {
+    playSound(bugSound);
+  }
+  stopGameTimer();
+  stopSound(bgSound);
   showPopUpWithText(win ? 'YOU WON !' : 'TRY AGAIN !');
 }
 
@@ -59,6 +74,7 @@ function showStopButton() {
   const icon = gameBtn.querySelector('.fas');
   icon.classList.add('fa-stop');
   icon.classList.remove('fa-play');
+  gameBtn.style.visibility = 'visible';
 }
 
 function hideGameButton() {
@@ -118,17 +134,24 @@ function onFieldClick(event) {
   if (target.matches('.carrot')) {
     target.remove();
     score++;
+    playSound(carrotSound);
+    updateScoreBoard();
     if (score === CARROT_COUNT) {
       finishGame(true);
     }
-    updateScoreBoard();
   } else if (target.matches('.bug')) {
-    stopGameTimer();
     finishGame(false);
   }
 }
 
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
 
+function stopSound(sound) {
+  sound.pause();
+}
 
 function updateScoreBoard() {
   gameScore.innerText = CARROT_COUNT - score;
